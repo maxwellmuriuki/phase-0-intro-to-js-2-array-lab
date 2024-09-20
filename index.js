c6f4737414da443dcf8fca2a882456639e9f4b4a
Your Name <usamax089@gmail.com>
2024-09-20 14:15:24 +0300

    first commit

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Write your solution here!
+let cats=["Milo", "Otis", "Garfield"]
+ function   destructivelyAppendCat(name){
+    cats.push("Ralph")
+ }
+ function  destructivelyPrependCat(name){
+    cats.unshift("Bob")
+ }
+ function destructivelyRemoveLastCat(){
+    cats.pop()
+ }
+function destructivelyRemoveFirstCat(){
+    cats.shift()
+}
+function appendCat(name){
+    let newcats=cats.slice();
+    newcats.push("Broom");
+    return  newcats;
+}
+function  prependCat(name){
+    const newCats = cats.slice();
+    newCats.unshift("Arnold");
+    return newCats;
+}
+
+
+function removeFirstCat(){
+    const newCats = cats.slice();
+    newCats.shift();
+    return newCats;
+}
+function   removeLastCat(){
+    let newCats=cats.slice();
+    newCats.pop();
+    return newCats;
+}
